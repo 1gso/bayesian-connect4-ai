aac52134b50b0d85bf26bb17ab7c3016091dc081
1gso <info@carmotcapital.com>
2025-06-02 11:26:25 -0700

    Add passphrase-protected SSH keys and modified download script to include SCP

--- get_data.sh
+++ get_data.sh
@@ -2,4 +2,4 @@
 # get_data.sh - Download training data from SFTP server
 
 mkdir -p ~/.ssh data/input && cp ssh_keys/bayesian-project-key ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
-for f in r139_a.txt r139_8.txt; do curl -k --user "Parmot:" --key ~/.ssh/id_rsa -o "data/input/$f" "sftp://24.23.247.209:22/$f"; done
+for f in r139_a.txt r139_8.txt; do scp -i ~/.ssh/id_rsa Parmot@24.23.247.209:$f data/input/; done
