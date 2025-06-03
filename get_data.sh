44f85984e9ca82b1f6c9c134f4570f7f001bab6c
1gso <info@carmotcapital.com>
2025-06-02 18:56:37 -0700

    feat: Add data download script and SSH keys

--- get_data.sh
+++ get_data.sh
@@ -2,4 +2,9 @@
 # get_data.sh - Download training data from SFTP server
 
 mkdir -p ~/.ssh data/input && cp ssh_keys/bayesian-project-key ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
-scp -i ~/.ssh/id_rsa Parmot@24.23.247.209:'{r139_a.txt,r139_8.txt}' data/input/
+
+echo "Downloading r139 data files..."
+scp -i ~/.ssh/id_rsa Parmot@24.23.247.209:r139_*.txt data/input/
+
+echo "Files downloaded:"
+ls -la data/input/r139*
